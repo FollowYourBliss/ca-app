d2d48143098c923d8e90c6913d09c187f980fea0
Wojciech Bozek <wbozek@fractalsoft.org>
2012-12-18 16:08:13 +0100

    Report initialization

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,9 +1,9 @@
-require "ca"
+
 class ResultsController < ApplicationController
+
   # GET /results
   # GET /results.json
   def index
-
   end
 
   # GET /results/1
@@ -15,7 +15,7 @@ class ResultsController < ApplicationController
   # GET /results/new
   # GET /results/new.json
   def new
-
+    puts " ania kokokokokokokokok"
   end
 
   # GET /results/1/edit
@@ -26,7 +26,15 @@ class ResultsController < ApplicationController
   # POST /results
   # POST /results.json
   def create
+    if params[:sample]
+      analyse = Ca::Analyse.new(HTMLReader.instance.page(params[:sample][:address]));
+    end
+    if params[:text]
+      analyse = Ca::Analyse.new(params[:text][:content]);
+    end
 
+      @problems = analyse.description.problems
+      @text = analyse.description.text
   end
 
   # PUT /results/1
